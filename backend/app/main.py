b88c305a9c8e42507c712c2fbb1070152490005e
badie16 <badi3bahida16@gmail.com>
2025-07-14 15:05:33 +0100

    feat: enhance API initialization

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
-app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+from app.database import Base, engine
+from app.routes import user  # routes/user.py
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="PhytoVigil API",
+    description="API backend de l'application PhytoVigil",
+    version="1.0.0"
+)
+
+app.include_router(user.router, prefix="/api", tags=["Users"])
+
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
 
 @app.get("/")
-def read_root():
-    return {"message": "Bienvenue sur l’API PhytoVigil 🌿"}
+def index():
+    return {"message": "Welcome to the PhytoVigil API"}
