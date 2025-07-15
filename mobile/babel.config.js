a27de9c346ffb91d431201728427b759bd41d393
badie16 <badi3bahida16@gmail.com>
2025-07-15 10:04:20 +0100

    feat: configure babel to use react-native-dotenv for environment variable support

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,5 +2,13 @@ module.exports = (api) => {
     api.cache(true)
     return {
         presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
+        plugins: [
+            ["module:react-native-dotenv", {
+                moduleName: "@env",
+                path: ".env",
+                safe: false,
+                allowUndefined: true,
+            }]
+        ]
     }
 }
