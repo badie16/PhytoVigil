78a0d8f3179e345d3155e82411bdf61fca49a85b
badie16 <badi3bahida16@gmail.com>
2025-07-17 15:39:39 +0100

    feat: add getDiseaseByName method to DiseaseService for fetching disease details by name

--- mobile/services/diseaseService.ts
+++ mobile/services/diseaseService.ts
@@ -1,9 +1,16 @@
-import type { Disease } from "@/types/index" 
 import { config } from "@/lib/config/env"
+import type { Disease } from "@/types/index"
 
 class DiseaseService {
-    async getAllDisease(skip=0,limit=100): Promise<Disease[]>{
+    async getAllDisease(skip = 0, limit = 100): Promise<Disease[]> {
         const res = await fetch(`${config.API_URL}/api/diseases/?skip=${skip}&limit=${limit}`)
+        if (!res.ok) {
+            throw new Error("Erreur lors de la récupération des maladies")
+        }
+        return res.json()
+    }
+    async getDiseaseByName(name: string): Promise<Disease> {
+        const res = await fetch(`${config.API_URL}/api/diseases/name/${name}`)
         if (!res.ok) {
             throw new Error("Erreur lors de la récupération des maladies")
         }
