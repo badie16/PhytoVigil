5c8666059a1bf58f3ffa9189c04dd3f864783ce7
badie16 <badi3bahida16@gmail.com>
2025-07-21 09:47:36 +0100

    feat: add TOKEN_KEY to environment configuration for authentication

--- mobile/lib/config/env.ts
+++ mobile/lib/config/env.ts
@@ -2,10 +2,12 @@ import Constants from 'expo-constants';
 const ENV = Constants.expoConfig?.extra ?? {};
 type AppConfig = {
     API_URL: string;
+    TOKEN_KEY: string
 };
 
 const getEnv = (): AppConfig => ({
-    API_URL: ENV.API_URL ?? "http://127.0.0.1:8000",
+    API_URL: "http://127.0.0.1:8000",
+    TOKEN_KEY: ENV.TOKEN_KEY ?? "auth_token"
 });
 
 export const config: AppConfig = getEnv();
