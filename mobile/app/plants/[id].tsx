892ac2a05436c626d25fc5445ce6fb49e2a06b22
badie16 <badi3bahida16@gmail.com>
2025-07-21 12:57:52 +0100

    refactor: simplify PlantDetailScreen by removing unused icons and integrating PlantUtils for icon and date formatting

--- mobile/app/plants/[id].tsx
+++ mobile/app/plants/[id].tsx
@@ -1,23 +1,17 @@
 import LoadingSpinner from '@/components/ui/loading-spinner';
+import { DateUtils } from '@/lib/constant/dateUtils';
 import { PlantUtils } from '@/lib/constant/plantUtils';
 import plantService from '@/services/remote/plantService';
 import { Plant, PlantScan } from '@/types';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import {
     Activity,
-    CircleAlert as AlertCircle,
-    Apple,
-    CircleCheck as CheckCircle,
     Clock,
     FileText,
-    Flower,
-    CircleHelp as HelpCircle,
-    Leaf,
     MapPin
 } from 'lucide-react-native';
 import React, { useEffect, useState } from 'react';
 import {
-    Dimensions,
     Image,
     ScrollView,
     StyleSheet,
@@ -25,35 +19,6 @@ import {
     TouchableOpacity,
     View
 } from 'react-native';
-
-const { width } = Dimensions.get('window');
-
-const getPlantIcon = (type: string) => {
-    switch (type.toLowerCase()) {
-        case 'vegetable':
-            return <Leaf size={24} color="#10B981" />;
-        case 'flower':
-            return <Flower size={24} color="#F59E0B" />;
-        case 'fruit':
-            return <Apple size={24} color="#10B981" />;
-        default:
-            return <Leaf size={24} color="#10B981" />;
-    }
-};
-
-const getStatusIcon = (status: PlantScan['status']) => {
-    switch (status) {
-        case 'healthy':
-            return <CheckCircle size={20} color="#10B981" />;
-        case 'diseased':
-            return <AlertCircle size={20} color="#EF4444" />;
-        case 'unknown':
-            return <HelpCircle size={20} color="#6B7280" />;
-        default:
-            return <HelpCircle size={20} color="#6B7280" />;
-    }
-};
-
 
 export default function PlantDetailScreen() {
     const router = useRouter();
@@ -79,12 +44,9 @@ export default function PlantDetailScreen() {
         }
         fetchPlants()
     }, [])
-    // const plantId = Array.isArray(id) ? parseInt(id[0], 10) : parseInt(id as string, 10);
-    // const plant = plants.find(p => p.id === plantId);
-
-    // const plantScans = mockPlantScans.filter(scan =>
-    //     scan.plantName.toLowerCase() === plant?.name.toLowerCase()
-    // );
+    const handleScanPress = (id: number) => {
+        router.push(`/plants/scan/${id}`);
+    }
     if (loading) {
         return (
             <LoadingSpinner message="" size={30} ></LoadingSpinner>
@@ -111,16 +73,16 @@ export default function PlantDetailScreen() {
                         resizeMode="cover"
                     />
                     <View className="absolute bottom-0 left-0 right-0 bg-black/60 p-5 flex-row items-center">
-                        <View className="w-14 h-14 rounded-full bg-white/20 justify-center items-center mr-4">
-                            {getPlantIcon(plant.type)}
+                        <View className="w-14 h-14 rounded-full bg-white/20 justify-center items-center mr-4"
+                        >
+                            {PlantUtils.getPlantIcon(plant.type, PlantUtils.getHealthColor(plant.health), 25)}
                         </View>
                         <View className="flex-1">
                             <Text className="text-2xl font-bold text-white mb-1">{plant.name}</Text>
                             <Text className="text-base text-slate-200 mb-2">{plant.variety || plant.type}</Text>
-                            <View style={[styles.healthBadge, { backgroundColor: PlantUtils.getHealthColor(plant.health) }]}> {/* à convertir plus tard */}
+                            <View style={[styles.healthBadge, { backgroundColor: PlantUtils.getHealthColor(plant.health) }]}>
                                 <Text className="text-xs font-semibold text-white">
-                                    {plant.health === 'healthy' ? 'Healthy' :
-                                        plant.health === 'warning' ? 'Needs Attention' : 'Critical'}
+                                    {PlantUtils.getHealthLabel(plant.health)}
                                 </Text>
                             </View>
                         </View>
@@ -134,24 +96,24 @@ export default function PlantDetailScreen() {
                         <View className="bg-white p-5 rounded-2xl shadow-sm space-y-4">
                             <View className="flex-row justify-between items-center">
                                 <Text className="text-base text-slate-500 font-medium">Type:</Text>
-                                <Text className="text-base text-slate-900 font-semibold text-right flex-1">{plant.type}</Text>
+                                <Text className="text-base text-slate-900 font-semibold text-right flex-1 capitalize">{plant.type}</Text>
                             </View>
                             {plant.variety && (
                                 <View className="flex-row justify-between items-center">
                                     <Text className="text-base text-slate-500 font-medium">Variety:</Text>
-                                    <Text className="text-base text-slate-900 font-semibold text-right flex-1">{plant.variety}</Text>
+                                    <Text className="text-base text-slate-900 font-semibold text-right flex-1 capitalize">{plant.variety}</Text>
                                 </View>
                             )}
                             {plant.lastScanned && (
                                 <View className="flex-row justify-between items-center">
                                     <Text className="text-base text-slate-500 font-medium">Planted:</Text>
-                                    <Text className="text-base text-slate-900 font-semibold text-right flex-1">{PlantUtils.formatFullDate(plant.createdAt)}</Text>
+                                    <Text className="text-base text-slate-900 font-semibold text-right flex-1 capitalize">{DateUtils.formatFullDate(plant.createdAt)}</Text>
                                 </View>
                             )}
                             {plant.location?.address && (
                                 <View className="flex-row justify-between items-center">
                                     <Text className="text-base text-slate-500 font-medium">Location:</Text>
-                                    <Text className="text-base text-slate-900 font-semibold text-right flex-1">{plant.location.address}</Text>
+                                    <Text className="text-base text-slate-900 font-semibold text-right flex-1 capitalize">{plant.location.address}</Text>
                                 </View>
                             )}
                         </View>
@@ -162,7 +124,7 @@ export default function PlantDetailScreen() {
                         <View className="space-y-3">
                             <Text className="text-xl font-semibold text-slate-900">Notes</Text>
                             <View className="bg-white p-5 rounded-2xl shadow-sm flex-row space-x-3">
-                                <FileText size={16} color="#6B7280" />
+                                <FileText size={18} className='mt-[2px]' color="#6B7280" />
                                 <Text className="text-base text-slate-700 flex-1 leading-6">{plant.notes}</Text>
                             </View>
                         </View>
@@ -180,15 +142,15 @@ export default function PlantDetailScreen() {
                         {plantScans.length > 0 ? (
                             <View className="space-y-4">
                                 {plantScans.map((scan) => (
-                                    <View key={scan.id} className="bg-white rounded-2xl p-5 shadow-sm">
+                                    <TouchableOpacity key={scan.id} className="bg-white rounded-2xl p-5 shadow-sm" onPress={() => handleScanPress(scan.id)}>
                                         <View className="flex-row justify-between items-center mb-3">
                                             <View className="flex-row items-center space-x-2">
-                                                {getStatusIcon(scan.status)}
+                                                {PlantUtils.getStatusIcon(scan.status)}
                                                 <Text style={[styles.scanStatusText, { color: PlantUtils.getHealthColor(scan.status) }]}>
                                                     {scan.diseaseName}
                                                 </Text>
                                             </View>
-                                            <Text className="text-sm text-slate-500">{PlantUtils.formatDate(scan.createdAt)}</Text>
+                                            <Text className="text-sm text-slate-500">{DateUtils.formatDate(scan.createdAt)}</Text>
                                         </View>
 
                                         <View className="space-y-2 mb-3">
@@ -205,7 +167,7 @@ export default function PlantDetailScreen() {
                                             )}
                                         </View>
 
-                                        {scan.treatment && (
+                                        {/* {scan.treatment && (
                                             <View className="bg-green-50 p-4 rounded-xl mb-3">
                                                 <Text className="text-sm font-semibold text-green-600 mb-2">Treatment:</Text>
                                                 <Text className="text-sm text-slate-700 leading-5">{scan.treatment}</Text>
@@ -217,8 +179,8 @@ export default function PlantDetailScreen() {
                                                 <Text className="text-sm font-semibold text-slate-700 mb-2">Notes:</Text>
                                                 <Text className="text-sm text-slate-500 leading-5">{scan.notes}</Text>
                                             </View>
-                                        )}
-                                    </View>
+                                        )} */}
+                                    </TouchableOpacity>
                                 ))}
                             </View>
                         ) : (
