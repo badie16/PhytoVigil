a3f4155c2898268d0b3cd99a0d4013a064bdff76
badie16 <badi3bahida16@gmail.com>
2025-07-29 11:10:42 +0100

    feat: update metro.config.js to support SVG transformations and enhance NativeWind integration

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,6 +1,22 @@
-const { getDefaultConfig } = require("expo/metro-config")
-const { withNativeWind } = require("nativewind/metro")
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./assets/style/global.css" })
+// Corriger proprement les extensions SVG et require.context
+config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    unstable_allowRequireContext: true,
+};
+
+config.resolver = {
+    ...config.resolver,
+    assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...config.resolver.sourceExts, "svg"],
+};
+
+// Applique NativeWind correctement
+module.exports = withNativeWind(config, {
+    input: "./assets/style/global.css",
+});
